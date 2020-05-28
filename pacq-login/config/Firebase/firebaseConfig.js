949aaebe6172380d0efd9377e133c4715362be2c
Nejirwan <37329734+NRiCe31@users.noreply.github.com>
2020-05-28 15:17:53 +0200

    Add files via upload

---
+++ pacq-login/config/Firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyDgClIzb6AkkqJJtdoEfwXgCmVyOmm2IQA",
+    authDomain: "react-login-1ba23.firebaseapp.com",
+    databaseURL: "https://react-login-1ba23.firebaseio.com",
+    projectId: "react-login-1ba23",
+    storageBucket: "react-login-1ba23.appspot.com",
+    messagingSenderId: "762310558817",
+    appId: "1:762310558817:web:ee8b05803ebb59561b1620",
+    measurementId: "G-1KZ4CT9NC9"
+  }
